68057d162d89b91db6c671b3414adc2e0bc2eba1
noooooooooooooooob <pisas@naver.com>
2025-03-08 02:02:58 +0900

    카드 드래그 카드 교체 커밋

---
+++ Assets/Scripts/Cards/CardZone.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class CardZone : MonoBehaviour
+{
+    public Type type;
+    public Type getType(){
+        return type;
+    }
+}
--- Assets/Scripts/Cards/Player Card.cs
+++ Assets/Scripts/Cards/Player Card.cs
@@ -2,26 +2,77 @@ using UnityEngine;
 
 public class PlayerCard : MonoBehaviour
 {
+    [Header("-----------------Card Type")]
     public Type type;
     public int value;
+    private Vector3 defaultPos;
+    private Vector3 offset;
+    private Camera cam;
+    [SerializeField] private bool isInTargetZone;   
+    private Vector3 targetPos;
     void Awake()
     {
-        type = Type.CLOVER;
-        value = 1;
     }
 
-    public void pointerEnter()
+    void Start()
     {
-        Vector3 newPosition = transform.position;
-        newPosition.y += 0.1f;
-        transform.position = newPosition;
-        Debug.Log("pointer in");
+        cam = Camera.main; // 메인 카메라 참조
+        defaultPos = transform.position;
     }
-    public void pointOut()
+
+    void OnMouseDown()
     {
-        Vector3 newPosition = transform.position;
-        newPosition.y -= 0.1f;
-        transform.position = newPosition;
-        Debug.Log("pointer out");
+        offset = transform.position - GetMouseWorldPos();
+    }
+
+    void OnMouseDrag()
+    {
+        transform.position = GetMouseWorldPos() + offset;
+    }
+
+    void OnMouseUp()
+    {
+        if(isInTargetZone)
+        {
+            ClearTargetZone();
+            transform.position = targetPos;
+        }
+        else transform.position = defaultPos;
+    }
+
+    Vector3 GetMouseWorldPos()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+        mousePoint.z = 0f;
+        return cam.ScreenToWorldPoint(mousePoint);
+    }
+    // 특정 영역에 들어왔는지 감지하는 메서드
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag== "TargetZone" && collision.GetComponent<CardZone>().getType() == type) // 특정 태그를 가진 오브젝트와 충돌 시
+        {
+            isInTargetZone = true;
+            targetPos = collision.transform.position;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("TargetZone"))
+        {
+            isInTargetZone = false;
+        }
+    }
+    void ClearTargetZone()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(targetPos, 0.1f); // targetZone 근처에 있는 오브젝트 찾기
+        foreach (Collider2D col in colliders)
+        {
+            PlayerCard existingCard = col.GetComponent<PlayerCard>();
+            if (existingCard != null && existingCard != this)
+            {
+                existingCard.transform.position = existingCard.defaultPos; // 기존 카드 원래 자리로 복귀
+            }
+        }
     }
 }
